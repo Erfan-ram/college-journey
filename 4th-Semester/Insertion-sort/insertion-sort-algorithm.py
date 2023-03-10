13ad60e8ffbca971ab0bd852b99dff17cf291388
Erfan-ram <Erfanramezany245@gmail.com>
2023-03-10 23:57:06 +0330

    added code

--- 4th-Semester/Insertion-sort/insertion-sort-algorithm.py
+++ 4th-Semester/Insertion-sort/insertion-sort-algorithm.py
@@ -1,7 +1,25 @@
 # Insertion sort is a simple sorting algorithm that works similar to the way you sort
-# playing cards in your hands. The array is virtually split into a sorted and
-# an unsorted part. Values from the unsorted part are picked and placed at the correct 
+# playing cards in your hands. The arrayay is virtually split into a sorted and
+# an unsorted part. Values from the unsorted part are picked and placed at the correct
 # position in the sorted part.
 
+
+# Python program to implement Insertion Sort
 def insertion_sort(array):
-    
+    lenght = len(array)
+
+    for i in range(1, lenght):
+        key = array[i]
+        j = i-1
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key
+
+    return array
+
+
+test_array = insertion_sort([50, 26, 25, 3, 5, 9])
+
+for i in test_array:
+    print(f"{i}",end=" ")
