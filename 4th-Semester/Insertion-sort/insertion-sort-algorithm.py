70aba715a281066fb01c39b4d9d423828cb82ae8
Erfan-ram <Erfanramezany245@gmail.com>
2023-03-11 00:48:43 +0330

    colored text

--- 4th-Semester/Insertion-sort/insertion-sort-algorithm.py
+++ 4th-Semester/Insertion-sort/insertion-sort-algorithm.py
@@ -1,10 +1,15 @@
+# colored text
+import sys
+RED = "\033[1;31m"
+RESET = "\033[0m"
+YELLOW = "\033[1;33m"
+
 # Insertion sort is a simple sorting algorithm that works similar to the way you sort
 # playing cards in your hands. The arrayay is virtually split into a sorted and
 # an unsorted part. Values from the unsorted part are picked and placed at the correct
 # position in the sorted part.
 
 
-# Python program to implement Insertion Sort
 def insertion_sort(array):
     lenght = len(array)
 
@@ -21,8 +26,9 @@ def insertion_sort(array):
     return array
 
 
-print("Please enter a list of numbers in a row seperating with \'-\' to get sorted")
-ask = input("\t (num-num-num-num)      UR num:  ")
+print(
+    f"{RED}Please enter a list of numbers in a row seperating with \'-\' to get sorted{RESET}")
+ask = input(f"\t (num-num-num-num)      {YELLOW}UR num:{RESET}  ")
 
 # convert string to list of numbers
 test_array = ask.split('-')
@@ -30,5 +36,6 @@ test_array = list(map(lambda x: int(x), test_array))
 
 sorted = insertion_sort(test_array)
 
+print("\nsorted numbers :  ")
 for i in test_array:
-    print(f"{i}", end=" ")
+    print(f"{YELLOW}{i}", end=" ")
