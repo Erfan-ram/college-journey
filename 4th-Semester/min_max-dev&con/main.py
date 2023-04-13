6c07c06402888daf18d072a4ce9111aa4a8c552f
Erfan ram <Erfanramezany245@gmail.com>
2023-04-13 18:55:27 +0330

    added some text

--- 4th-Semester/min_max-dev&con/main.py
+++ 4th-Semester/min_max-dev&con/main.py
@@ -1,4 +1,16 @@
+# required libraries
+import pandas as pd
+from time import time as record
+
 from minmax_algorithm import find_min_max
 
-while (1):
-    pass
+
+class main():
+    def __init__(self) -> None:
+        # print("Hi please enter list size to compare duration time")
+        print(
+            "this program would compare finding duration time that is needed by devide and")
+        print("conquer algorithm to find min and max in a list")
+        print("and the factor is needed to compare is list size")
+
+        list_size = input("\tnow enter list size : ")
